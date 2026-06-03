f04a679f166dd5136e108ea69ab338ee10a2dedb
Siddharth Jha <siddharthjha79348@gmail.com>
2026-06-03 16:23:03 +0530

    fix: use dummy stripe secret key for build evaluation

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe';
 
-const secretKey = process.env.STRIPE_SECRET_KEY || '';
+const secretKey = process.env.STRIPE_SECRET_KEY || 'sk_test_dummy';
 
 export const stripe = new Stripe(secretKey, {
   apiVersion: '2025-01-27-pre.0' as any, // standard server compatibility
